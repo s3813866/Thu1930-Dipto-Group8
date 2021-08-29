3e0639fa0ac49cd0aac142050675d70689b9dd56
s3744264 <s3744264@student.rmit.edu.au>
2021-08-29 23:08:16 +1000

    Initialised book microservice

---
+++ BackEnd/bookmicroservices/src/main/java/com/rmit/sept/bookmicroservices/BookmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.bookmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmicroservicesApplication.class, args);
+    }
+
+}
