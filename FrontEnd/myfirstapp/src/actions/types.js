c56baadd79865d07b068f38328d4877df01b1c41
s3813866 <s3813866@student.rmit.edu.au>
2021-09-28 16:27:30 +0800

    Update types.js

--- FrontEnd/myfirstapp/src/actions/types.js
+++ FrontEnd/myfirstapp/src/actions/types.js
@@ -2,12 +2,16 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_PERSONS = "GET_PERSONS";
 export const GET_PERSON = "GET_PERSON";
 
+export const BAN_PERSON = "BAN_PERSON";
+export const UNBAN_PERSON = "UNBAN_PERSON";
+
 export const GET_BOOK = "GET_BOOK";
 export const GET_BOOKS = "GET_BOOKS";
 
 export const GET_AUTHOR = "GET_AUTHOR";
 
-
+export const GET_ENQUIRIES = "GET_ENQUIRIES";
+export const DELETE_ENQUIRY = "DELETE_ENQUIRY";
 
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
